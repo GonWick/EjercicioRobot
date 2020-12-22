8b68e32af39496a0a71b47d6bccd9f3f53b27d77
GonWick <gonxza@gmail.com>
2020-12-22 13:58:38 +0100

    Test 2 OK

--- EjercicioRobot/Form1.Designer.vb
+++ EjercicioRobot/Form1.Designer.vb
@@ -24,18 +24,26 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.GroupBoxTest1 = New System.Windows.Forms.GroupBox()
-        Me.RadioButtonVenus = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonDiaMartes = New System.Windows.Forms.RadioButton()
         Me.ButtonSalir = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.RadioButtonLuna = New System.Windows.Forms.RadioButton()
         Me.RadioButtonMarte = New System.Windows.Forms.RadioButton()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBoxTest2 = New System.Windows.Forms.GroupBox()
+        Me.LabelRespuesta = New System.Windows.Forms.Label()
+        Me.LabelFraseInfo = New System.Windows.Forms.Label()
+        Me.TextBoxFrase = New System.Windows.Forms.TextBox()
+        Me.ButtonFrase = New System.Windows.Forms.Button()
+        Me.RadioButtonPlaton = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonDescartes = New System.Windows.Forms.RadioButton()
         Me.GroupBoxTest1.SuspendLayout()
+        Me.GroupBoxTest2.SuspendLayout()
         Me.SuspendLayout()
         '
         'GroupBoxTest1
         '
-        Me.GroupBoxTest1.Controls.Add(Me.RadioButtonVenus)
+        Me.GroupBoxTest1.Controls.Add(Me.RadioButtonDiaMartes)
         Me.GroupBoxTest1.Controls.Add(Me.ButtonSalir)
         Me.GroupBoxTest1.Controls.Add(Me.Label1)
         Me.GroupBoxTest1.Controls.Add(Me.RadioButtonLuna)
@@ -48,16 +56,16 @@ Partial Class Form1
         Me.GroupBoxTest1.TabStop = False
         Me.GroupBoxTest1.Text = "Test 1"
         '
-        'RadioButtonVenus
+        'RadioButtonDiaMartes
         '
-        Me.RadioButtonVenus.AutoSize = True
-        Me.RadioButtonVenus.Location = New System.Drawing.Point(117, 76)
-        Me.RadioButtonVenus.Name = "RadioButtonVenus"
-        Me.RadioButtonVenus.Size = New System.Drawing.Size(61, 19)
-        Me.RadioButtonVenus.TabIndex = 11
-        Me.RadioButtonVenus.TabStop = True
-        Me.RadioButtonVenus.Text = "VENUS"
-        Me.RadioButtonVenus.UseVisualStyleBackColor = True
+        Me.RadioButtonDiaMartes.AutoSize = True
+        Me.RadioButtonDiaMartes.Location = New System.Drawing.Point(117, 76)
+        Me.RadioButtonDiaMartes.Name = "RadioButtonDiaMartes"
+        Me.RadioButtonDiaMartes.Size = New System.Drawing.Size(68, 19)
+        Me.RadioButtonDiaMartes.TabIndex = 11
+        Me.RadioButtonDiaMartes.TabStop = True
+        Me.RadioButtonDiaMartes.Text = "MARTES"
+        Me.RadioButtonDiaMartes.UseVisualStyleBackColor = True
         '
         'ButtonSalir
         '
@@ -75,9 +83,9 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(30, 48)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(221, 15)
+        Me.Label1.Size = New System.Drawing.Size(237, 15)
         Me.Label1.TabIndex = 9
-        Me.Label1.Text = "A donde llego el Robot Curiosity en 2012"
+        Me.Label1.Text = "¿ A donde llego el Robot Curiosity en 2012 ?"
         '
         'RadioButtonLuna
         '
@@ -113,11 +121,83 @@ Partial Class Form1
         Me.Button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight
         Me.Button1.UseVisualStyleBackColor = True
         '
+        'GroupBoxTest2
+        '
+        Me.GroupBoxTest2.Controls.Add(Me.LabelRespuesta)
+        Me.GroupBoxTest2.Controls.Add(Me.LabelFraseInfo)
+        Me.GroupBoxTest2.Controls.Add(Me.TextBoxFrase)
+        Me.GroupBoxTest2.Controls.Add(Me.ButtonFrase)
+        Me.GroupBoxTest2.Controls.Add(Me.RadioButtonPlaton)
+        Me.GroupBoxTest2.Controls.Add(Me.RadioButtonDescartes)
+        Me.GroupBoxTest2.Location = New System.Drawing.Point(330, 18)
+        Me.GroupBoxTest2.Name = "GroupBoxTest2"
+        Me.GroupBoxTest2.Size = New System.Drawing.Size(278, 235)
+        Me.GroupBoxTest2.TabIndex = 7
+        Me.GroupBoxTest2.TabStop = False
+        Me.GroupBoxTest2.Text = "Test 2"
+        '
+        'LabelRespuesta
+        '
+        Me.LabelRespuesta.AutoSize = True
+        Me.LabelRespuesta.Location = New System.Drawing.Point(27, 199)
+        Me.LabelRespuesta.Name = "LabelRespuesta"
+        Me.LabelRespuesta.Size = New System.Drawing.Size(78, 15)
+        Me.LabelRespuesta.TabIndex = 5
+        Me.LabelRespuesta.Text = "Frase Famosa"
+        '
+        'LabelFraseInfo
+        '
+        Me.LabelFraseInfo.AutoSize = True
+        Me.LabelFraseInfo.Location = New System.Drawing.Point(29, 42)
+        Me.LabelFraseInfo.Name = "LabelFraseInfo"
+        Me.LabelFraseInfo.Size = New System.Drawing.Size(147, 15)
+        Me.LabelFraseInfo.TabIndex = 4
+        Me.LabelFraseInfo.Text = "Escriba Descartes o Platón:"
+        '
+        'TextBoxFrase
+        '
+        Me.TextBoxFrase.Location = New System.Drawing.Point(29, 72)
+        Me.TextBoxFrase.Name = "TextBoxFrase"
+        Me.TextBoxFrase.Size = New System.Drawing.Size(100, 23)
+        Me.TextBoxFrase.TabIndex = 3
+        '
+        'ButtonFrase
+        '
+        Me.ButtonFrase.Location = New System.Drawing.Point(29, 151)
+        Me.ButtonFrase.Name = "ButtonFrase"
+        Me.ButtonFrase.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonFrase.TabIndex = 2
+        Me.ButtonFrase.Text = "Ver Frase Famosa"
+        Me.ButtonFrase.UseVisualStyleBackColor = True
+        '
+        'RadioButtonPlaton
+        '
+        Me.RadioButtonPlaton.AutoSize = True
+        Me.RadioButtonPlaton.Location = New System.Drawing.Point(29, 101)
+        Me.RadioButtonPlaton.Name = "RadioButtonPlaton"
+        Me.RadioButtonPlaton.Size = New System.Drawing.Size(59, 19)
+        Me.RadioButtonPlaton.TabIndex = 1
+        Me.RadioButtonPlaton.TabStop = True
+        Me.RadioButtonPlaton.Text = "Platon"
+        Me.RadioButtonPlaton.UseVisualStyleBackColor = True
+        '
+        'RadioButtonDescartes
+        '
+        Me.RadioButtonDescartes.AutoSize = True
+        Me.RadioButtonDescartes.Location = New System.Drawing.Point(29, 126)
+        Me.RadioButtonDescartes.Name = "RadioButtonDescartes"
+        Me.RadioButtonDescartes.Size = New System.Drawing.Size(75, 19)
+        Me.RadioButtonDescartes.TabIndex = 0
+        Me.RadioButtonDescartes.TabStop = True
+        Me.RadioButtonDescartes.Text = "Descartes"
+        Me.RadioButtonDescartes.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(623, 276)
+        Me.Controls.Add(Me.GroupBoxTest2)
         Me.Controls.Add(Me.GroupBoxTest1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -128,15 +208,25 @@ Partial Class Form1
         Me.Text = "EJERCICIO ROBOT"
         Me.GroupBoxTest1.ResumeLayout(False)
         Me.GroupBoxTest1.PerformLayout()
+        Me.GroupBoxTest2.ResumeLayout(False)
+        Me.GroupBoxTest2.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents GroupBoxTest1 As GroupBox
-    Friend WithEvents RadioButtonVenus As RadioButton
+    Friend WithEvents RadioButtonDiaMartes As RadioButton
     Friend WithEvents ButtonSalir As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents RadioButtonLuna As RadioButton
     Friend WithEvents RadioButtonMarte As RadioButton
     Friend WithEvents Button1 As Button
+    Friend WithEvents GroupBoxTest2 As GroupBox
+    Friend WithEvents LabelFraseInfo As Label
+    Friend WithEvents TextBoxFrase As TextBox
+    Friend WithEvents ButtonFrase As Button
+    Friend WithEvents RadioButtonPlaton As RadioButton
+    Friend WithEvents RadioButtonDescartes As RadioButton
+    Friend WithEvents LabelFrase As Label
+    Friend WithEvents LabelRespuesta As Label
 End Class
--- EjercicioRobot/Form1.vb
+++ EjercicioRobot/Form1.vb
@@ -4,12 +4,51 @@
     Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
         If RadioButtonMarte.Checked = True Then
             MsgBox(" EL ROBOT CURIOSITY LLEGO A MARTE EN EL 2012",, "Vamos a Marte")
+        ElseIf RadioButtonDiaMartes.Checked = True Then
+            MsgBox(" Martes es un dia de la semana",, "Martes y 13! Bufff")
         Else
+
             MsgBox(" Vuelvelo a intentar",, "Respuesta incorrepta")
         End If
     End Sub
 
     Private Sub ButtonSalir_Click_1(sender As Object, e As EventArgs) Handles ButtonSalir.Click
         Close()
+    End Sub
+
+    Private Sub ButtonFrase_Click(sender As Object, e As EventArgs) Handles ButtonFrase.Click
+        Dim D As String = "Descartes"
+        Dim P As String = "Platon"
+        If TextBoxFrase.Text = D Or RadioButtonDescartes.Checked = True Then
+            LabelRespuesta.Text = "¡Pienso luego existo!"
+        ElseIf TextBoxFrase.Text = P Or RadioButtonPlaton.Checked = True Then
+            LabelRespuesta.Text = " ¡Solo sé que nada se!"
+        Else
+            MsgBox("Escriba Descartes o Platón para ver sus Frases famosas.",, "Texto Mal")
+        End If
+
+    End Sub
+
+    Private Sub TextBoxFrase_TextChanged(sender As Object, e As EventArgs) Handles TextBoxFrase.TextChanged
+
+
+    End Sub
+
+    Private Sub TextBoxFrase_Click(sender As Object, e As EventArgs) Handles TextBoxFrase.Click
+        TextBoxFrase.Text = ""
+        RadioButtonDescartes.Checked = False
+        RadioButtonPlaton.Checked = False
+    End Sub
+
+    Private Sub RadioButtonPlaton_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButtonPlaton.CheckedChanged
+        TextBoxFrase.Text = "Platon"
+    End Sub
+
+    Private Sub RadioButtonDescartes_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButtonDescartes.CheckedChanged
+        TextBoxFrase.Text = "Descartes"
+    End Sub
+
+    Private Sub TextBoxFrase_MouseClick(sender As Object, e As MouseEventArgs) Handles TextBoxFrase.MouseClick
+        TextBoxFrase.Text = ""
     End Sub
 End Class
